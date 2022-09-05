d2069e59bb0a3661ef253f8dd3b779fd1b8f8c15
Andrew Retherford <105827498+andrewretherford@users.noreply.github.com>
2022-09-05 16:21:23 -0700

    Adds functionality to carousel

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const professionalLinks = document.querySelector('.professional-links')
 const sitesOfInterest = document.querySelector('.sites-of-interest')
 const navbar = document.querySelector('.navbar')
 
+// Carousel
+const slideContainer = document.querySelector('.slide-container')
+const arrowContainer = document.querySelector('.arrow-container')
+const slide1 = document.querySelector('#slide1')
+const slide2 = document.querySelector('#slide2')
+const slide3 = document.querySelector('#slide3')
+const slide4 = document.querySelector('#slide4')
+
 // Modal
 const resumeModal = document.querySelector('#resume-modal')
 const resumeLink = document.querySelector('#resume-link')
@@ -26,6 +34,7 @@ const resumeLink = document.querySelector('#resume-link')
 // ------------------------------------------------------------ //
 
 let currentPage = homePageContainer
+let currentSlide = 1
 
 // Utility Functions
 // ------------------------------------------------------------ //
@@ -40,17 +49,65 @@ function navigate(destination, origin) {
     currentPage = destination
 }
 
-function expandOrShrink(element) {
-    if(element.classList.contains('shrink')) {
-        element.classList.toggle('shrink')
-        element.classList.toggle('expand')
-    } else if(element.classList.contains('expand')) {
-        element.classList.toggle('expand')
-        element.classList.toggle('shrink')
-    } else {
-        element.classList.toggle('expand')
+function setMainSlide(currentSlide, direction) {
+
+    function toggleSlide(slide) {
+        slide.classList.toggle('main')
+        slide.classList.toggle('side')
+    }
+
+    console.log(currentSlide)
+    switch(currentSlide) {
+        case 0:
+            console.log('case 0')
+            toggleSlide(slide2)
+            toggleSlide(slide1)
+            break
+
+        case 1:
+            console.log('case 1')
+            if(direction == 'left') {
+                toggleSlide(slide3)
+                toggleSlide(slide2)
+            } else {
+                toggleSlide(slide1)
+                toggleSlide(slide2)
+            }
+            break
+
+        case 2:
+            console.log('case 2')
+            if(direction == 'left') {
+                toggleSlide(slide4)
+                toggleSlide(slide3)
+            } else {
+                toggleSlide(slide2)
+                toggleSlide(slide3)
+            }
+            break
+
+        case 3:
+            console.log('case 3')
+            toggleSlide(slide3)
+            toggleSlide(slide4)
+            break
+
+        default:
+            break
     }
 }
+
+// function expandOrShrink(element) {
+//     if(element.classList.contains('shrink')) {
+//         element.classList.toggle('shrink')
+//         element.classList.toggle('expand')
+//     } else if(element.classList.contains('expand')) {
+//         element.classList.toggle('expand')
+//         element.classList.toggle('shrink')
+//     } else {
+//         element.classList.toggle('expand')
+//     }
+// }
 
 // Event Handlers
 // ------------------------------------------------------------ //
@@ -77,69 +134,93 @@ function navHandler(e) {
     }
 }
 
-function navExpandHandler(e) {
-    if(e.target.tagName != 'IMG') return;
+function carouselHandler(e) {
+    switch(e.target.id) {
+        case 'left':
+            if(currentSlide > 0) {
+                currentSlide--
+                setMainSlide(currentSlide, 'left')
+            }
+            break
+
+        case 'right':
+            if(currentSlide < 3) {
+                currentSlide++
+                setMainSlide(currentSlide, 'right')
+            }
+            break
+        
+        default:
+            break
+    }
+
+    const translateChange = (currentSlide) * -385 + 310
+    slideContainer.style.transform = `translate(${translateChange}px)`
+}
+
+// function navExpandHandler(e) {
+//     if(e.target.tagName != 'IMG') return;
     
-    let profExpandTimeout, siteExpandTimeout
+//     let profExpandTimeout, siteExpandTimeout
 
-    switch(e.target.id) {
+//     switch(e.target.id) {
 
-        case 'professional-links-nav':
-            showHide(professionalLinksNav)
-            showHide(professionalLinks)
-            let profChildren = professionalLinks.children
-            clearTimeout(profExpandTimeout)
-            for(let i=0; i < profChildren.length; i++) {
-                profExpandTimeout = setTimeout(expandOrShrink, 10, profChildren[i])
-            }
-            break
+//         case 'professional-links-nav':
+//             showHide(professionalLinksNav)
+//             showHide(professionalLinks)
+//             let profChildren = professionalLinks.children
+//             clearTimeout(profExpandTimeout)
+//             for(let i=0; i < profChildren.length; i++) {
+//                 profExpandTimeout = setTimeout(expandOrShrink, 10, profChildren[i])
+//             }
+//             break
 
-        case 'sites-of-interest-nav':
-            showHide(sitesOfInterestNav)
-            showHide(sitesOfInterest)
-            let siteChildren = sitesOfInterest.children
-            clearTimeout(siteExpandTimeout)
-            for(let i=0; i < siteChildren.length; i++) {
-                siteExpandTimeout = setTimeout(expandOrShrink, 10, siteChildren[i])
-            }
-            break
+//         case 'sites-of-interest-nav':
+//             showHide(sitesOfInterestNav)
+//             showHide(sitesOfInterest)
+//             let siteChildren = sitesOfInterest.children
+//             clearTimeout(siteExpandTimeout)
+//             for(let i=0; i < siteChildren.length; i++) {
+//                 siteExpandTimeout = setTimeout(expandOrShrink, 10, siteChildren[i])
+//             }
+//             break
 
-        default: return
-    }
-}
+//         default: return
+//     }
+// }
 
-function navShrinkHandler(e) {
-    if(e.target.tagName != 'DIV') return;
+// function navShrinkHandler(e) {
+//     if(e.target.tagName != 'DIV') return;
 
-    let profNavTimeout, profLinksTimeout, siteNavTimeout, siteTimeout
+//     let profNavTimeout, profLinksTimeout, siteNavTimeout, siteTimeout
 
-    switch(e.target.id) {
+//     switch(e.target.id) {
 
-        case 'professional-links-div': 
-            clearTimeout(profNavTimeout)
-            clearTimeout(profLinksTimeout)
-            profNavTimeout = setTimeout(showHide, 500, professionalLinksNav)
-            profLinksTimeout = setTimeout(showHide, 500, professionalLinks)
-            let profChildren = professionalLinks.children
-            for(let i=0; i < profChildren.length; i++) {
-                expandOrShrink(profChildren[i])
-            }
-            break
+//         case 'professional-links-div': 
+//             clearTimeout(profNavTimeout)
+//             clearTimeout(profLinksTimeout)
+//             profNavTimeout = setTimeout(showHide, 500, professionalLinksNav)
+//             profLinksTimeout = setTimeout(showHide, 500, professionalLinks)
+//             let profChildren = professionalLinks.children
+//             for(let i=0; i < profChildren.length; i++) {
+//                 expandOrShrink(profChildren[i])
+//             }
+//             break
 
-        case 'sites-of-interest-div':
-            clearTimeout(siteNavTimeout)
-            clearTimeout(siteTimeout)
-            siteNavTimeout = setTimeout(showHide, 500, sitesOfInterestNav)
-            siteTimeout = setTimeout(showHide, 500, sitesOfInterest)
-            let siteChildren = sitesOfInterest.children
-            for(let i=0; i < siteChildren.length; i++) {
-                expandOrShrink(siteChildren[i])
-            }
-            break
+//         case 'sites-of-interest-div':
+//             clearTimeout(siteNavTimeout)
+//             clearTimeout(siteTimeout)
+//             siteNavTimeout = setTimeout(showHide, 500, sitesOfInterestNav)
+//             siteTimeout = setTimeout(showHide, 500, sitesOfInterest)
+//             let siteChildren = sitesOfInterest.children
+//             for(let i=0; i < siteChildren.length; i++) {
+//                 expandOrShrink(siteChildren[i])
+//             }
+//             break
 
-        default: return
-    }
-}
+//         default: return
+//     }
+// }
 
 function resumeLinkHandler(e) {
     showHide(resumeModal)
@@ -188,6 +269,8 @@ function toHomeHandler(e) {
 // home menu center navs
 // homeMenuNavContainer.addEventListener('click', homeMenuNavHandler)
 navbar.addEventListener('click', navHandler)
+
+arrowContainer.addEventListener('click', carouselHandler)
 
 // expanding navs
 // professionalLinksNav.addEventListener('mouseenter', navExpandHandler)
